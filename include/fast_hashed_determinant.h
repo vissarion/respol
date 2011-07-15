caa921e975e01c2bbc15199545584e3ffd9f55a3
luis4a0 <luis.penaranda@gmx.com>
2011-07-15 11:58:45 +0000

    put license files and added headers to all sources

--- include/fast_hashed_determinant.h
+++ include/fast_hashed_determinant.h
@@ -1,3 +1,21 @@
+// Copyright 2011 National and Kapodistrian University of Athens, Greece.
+//
+// This file is part of respol.
+//
+// Respol is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// Respol is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+//
+// See the file COPYING.LESSER for the text of the GNU Lesser General
+// Public License.  If you did not receive this file along with respol, see
+// <http://www.gnu.org/licenses/>.
+
 #ifndef FAST_HASHED_DETERMINANT_H
 #define FAST_HASHED_DETERMINANT_H
 
